ab823f27b57b26ed8c5531e0f83cba69f7bd061f
Armando del Río <adrio.6180@gmail.com>
2024-11-04 12:03:26 -0600

    fix: fix hisr

--- zshrc/zshrc-file.sh
+++ zshrc/zshrc-file.sh
@@ -103,7 +103,7 @@ setopt share_history
 setopt hist_expire_dups_first
 setopt hist_ignore_dups
 setopt hist_verify
-setopt hist_ignore_space
+# setopt hist_ignore_space
 
 
 bindkey '^[[A' history-search-backward
