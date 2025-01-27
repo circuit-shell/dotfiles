f3fee25d9a4e7a8f5fdd12fb91782b81b32ebe1b
Armando del Río <adrio.6180@gmail.com>
2025-01-26 19:48:03 -0600

    feat: add nvim surround

---
+++ neovim/specvim/lua/adrio/plugins/substitution.lua
@@ -0,0 +1,29 @@
+return {
+	{
+		"gbprod/substitute.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local substitute = require("substitute")
+			substitute.setup({
+				default_file_explorer = true,
+			})
+
+			-- set keymaps
+			local keymap = vim.keymap
+
+			keymap.set("n", "s", substitute.operator, { desc = "Substitute with motion" })
+			keymap.set("n", "ss", substitute.line, { desc = "Substitute line" })
+			keymap.set("n", "S", substitute.eol, { desc = "Substitute to end of line" })
+			keymap.set("x", "s", substitute.visual, { desc = "Substitute in visual mode" })
+		end,
+	},
+	{
+		"kylechui/nvim-surround",
+		event = { "BufReadPre", "BufNewFile" },
+		version = "*",
+		config = true,
+	},
+	{
+		"tpope/vim-repeat",
+	},
+}
