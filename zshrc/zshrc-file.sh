0bbfdab15d2d43c82ba67fd404afe67b4aa00cfa
Armando del Río <adrio.6180@gmail.com>
2025-03-20 23:39:23 -0600

    fix: zsh, remove unused nvim distros

--- zshrc/zshrc-file.sh
+++ zshrc/zshrc-file.sh
@@ -5,6 +5,12 @@
  (cd ~/github/dotfiles-latest && git pull >/dev/null 2>&1) || echo "Failed to pull dotfiles"
 # #############################################################################
 
+#  # ############################################################################
+# SECTION: Misc config
+ENABLE_CORRECTION="false"
+COMPLETION_WAITING_DOTS="true"
+export LANG=en_US.UTF-8
+# ############################################################################
 
 # ###########################################################################
 # SECTION: SSH key configuration
@@ -19,10 +25,14 @@
 # fi
 # ###########################################################################
 
-
 # #############################################################################
 # SECTION: Autocompletion settings
 # These have to be on the top, I remember I had issues this if not
+# https://github.com/zsh-users/zsh-autosuggestions
+# Right arrow to accept suggestion
+if [ -f "$(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh" ]; then
+  source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+fi
 unset ZSH_AUTOSUGGEST_USE_ASYNC
 zmodload zsh/complist
 autoload -U compinit
@@ -48,19 +58,17 @@ zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d (errors: %e) -!
 zstyle ':completion:*:*:*:*:descriptions' format '%F{blue}-- %D %d --%f'
 zstyle ':completion:*:*:*:*:messages' format ' %F{purple} -- %d --%f'
 zstyle ':completion:*:*:*:*:warnings' format ' %F{red}-- no matches found --%f'
-# zstyle ':completion:*:default' list-prompt '%S%M matches%s'
+zstyle ':completion:*:default' list-prompt '%S%M matches%s'
 # Colors for files and directory
-# zstyle ':completion:*:*:*:*:default' list-colors '${(s.:.)LS_COLORS}'
+zstyle ':completion:*:*:*:*:default' list-colors '${(s.:.)LS_COLORS}'
 # #############################################################################
-
 
 # #############################################################################
 # SECTION: Imports
 source ~/github/dotfiles-latest/zshrc/helper/functions.sh
 source ~/github/dotfiles-latest/zshrc/helper/aliases.sh
-source ~/github/dotfiles-latest/zshrc/helper/g-plugin.sh
+source ~/github/dotfiles-latest/zshrc/helper/git-plugin.sh
 # #############################################################################
-
 
 # #############################################################################
 # SECTION: Create directories and symlinks
@@ -77,7 +85,6 @@ create_symlink ~/github/dotfiles-latest/.prettierrc.yaml ~/.prettierrc.yaml
 # create_symlink ~/github/dotfiles-latest/sketchybar/default ~/.config/sketchybar
 # create_symlink ~/github/dotfiles-latest/sketchybar/neutonfoo ~/.config/sketchybar
 # #############################################################################
-
 
 ## #############################################################################
 # SECTION: Detect OS
@@ -97,7 +104,6 @@ Linux)
 esac
 # #############################################################################
 
-
 # #############################################################################
 # Section: MacOS-specific configurations
 if [ "$OS" = 'Mac' ]; then
@@ -108,6 +114,12 @@ if [ "$OS" = 'Mac' ]; then
   # if command -v brew &>/dev/null; then
   #   eval "$(/opt/homebrew/bin/brew shellenv)"
   # fi
+  export HOMEBREW_NO_AUTO_UPDATE="1"
+  export PATH="$(brew --prefix)/bin:$PATH"
+  if command -v brew &>/dev/null; then
+    FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
+  fi
+  # ###########################################################################
 
   # ###########################################################################
   # SECTION: install xterm info
@@ -137,54 +149,29 @@ if [ "$OS" = 'Mac' ]; then
   install_xterm_kitty_terminfo
   # ############################################################################
 
-
   # ############################################################################
   # SECTION: nvim
-  create_symlink ~/github/dotfiles-latest/neovim/kickstart.nvim/ ~/.config/kickstart.nvim
-  create_symlink ~/github/dotfiles-latest/neovim/lazyvim/ ~/.config/lazyvim
   create_symlink ~/github/dotfiles-latest/neovim/specvim/ ~/.config/nvim
-  # You can use NVIM_APPNAME=nvim-NAME to maintain multiple configurations.
   v() {
-    export NVIM_APPNAME="nvim"
     $(brew --prefix)/bin/nvim
   }
+  # Set nvim as default editor
   export EDITOR="$(brew --prefix nvim)/bin/nvim"
   # Open man pages with nvim
   if command -v nvim &>/dev/null; then
     export MANPAGER='nvim +Man!'
     export MANWIDTH=999
   fi
-  # Set up vi mode
+  # ############################################################################
+
+  # ############################################################################
+  # SECTION: vi mode
   # test {really} long (command) using a { lot } of symbols {page} and {abc} and other ones [find] () "test page" {'command 2'}
   if [ -f "$(brew --prefix)/opt/zsh-vi-mode/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh" ]; then
     source $(brew --prefix)/opt/zsh-vi-mode/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
     bindkey -r '\e/'
-    # Following 4 lines modify the escape key to `kj`
-    ZVM_VI_ESCAPE_BINDKEY=kj
-    ZVM_VI_INSERT_ESCAPE_BINDKEY=$ZVM_VI_ESCAPE_BINDKEY
-    ZVM_VI_VISUAL_ESCAPE_BINDKEY=$ZVM_VI_ESCAPE_BINDKEY
-    ZVM_VI_OPPEND_ESCAPE_BINDKEY=$ZVM_VI_ESCAPE_BINDKEY
-
-    function zvm_after_lazy_keybindings() {
-      # Remap to go to the beginning of the line
-      zvm_bindkey vicmd 'gh' beginning-of-line
-      # Remap to go to the end of the line
-      zvm_bindkey vicmd 'gl' end-of-line
-    }
-
-    # NOTE: My cursor was not blinking when using wezterm with the "wezterm"
-    # terminfo, setting it to a blinking cursor below fixed that
-    # I also set my term to "xterm-kitty" for this to work
-    # This also specifies the blinking cursor
-    # ZVM_CURSOR_STYLE_ENABLED=false
-    ZVM_INSERT_MODE_CURSOR=$ZVM_CURSOR_BLINKING_BEAM
-    ZVM_NORMAL_MODE_CURSOR=$ZVM_CURSOR_BLINKING_BLOCK
-    ZVM_OPPEND_MODE_CURSOR=$ZVM_CURSOR_BLINKING_UNDERLINE
-    # Source .fzf.zsh so that the ctrl+r bindkey is given back fzf
-    zvm_after_init_commands+=('[ -f $HOME/.fzf.zsh ] && source $HOME/.fzf.zsh')
   fi
   # ###########################################################################
-
 
   # ###########################################################################
   # SECTION: sketchybar
@@ -193,47 +180,25 @@ if [ "$OS" = 'Mac' ]; then
   # Personally I added "list" and "install", and everything that is after but
   # that's just a personal preference.
   # That way sketchybar updates when I run those commands as well
-  if command -v sketchybar &>/dev/null; then
-    # When the zshrc file is ran, reload sketchybar, in case the theme was
-    # switched
-    # I disabled this as it was getting refreshed every time I opened the
-    # terminal and if I restored a lot of sessions after rebooting it was a mess
-    # sketchybar --reload
+  # if command -v sketchybar &>/dev/null; then
+  #   # When the zshrc file is ran, reload sketchybar, in case the theme was
+  #   # switched
+  #   # I disabled this as it was getting refreshed every time I opened the
+  #   # terminal and if I restored a lot of sessions after rebooting it was a mess
+  #   # sketchybar --reload
 
-    # Define a custom 'brew' function to wrap the Homebrew command.
-    function brew() {
-      # Execute the original Homebrew command with all passed arguments.
-      command brew "$@"
-      # Check if the command includes "upgrade", "update", or "outdated".
-      if [[ $* =~ "upgrade" ]] || [[ $* =~ "update" ]] || [[ $* =~ "outdated" ]] || [[ $* =~ "list" ]] || [[ $* =~ "install" ]] || [[ $* =~ "uninstall" ]] || [[ $* =~ "bundle" ]] || [[ $* =~ "doctor" ]] || [[ $* =~ "info" ]] || [[ $* =~ "cleanup" ]]; then
-        # If so, notify SketchyBar to trigger a custom action.
-        sketchybar --trigger brew_update
-      fi
-    }
-  fi
+  #   # Define a custom 'brew' function to wrap the Homebrew command.
+  #   function brew() {
+  #     # Execute the original Homebrew command with all passed arguments.
+  #     command brew "$@"
+  #     # Check if the command includes "upgrade", "update", or "outdated".
+  #     if [[ $* =~ "upgrade" ]] || [[ $* =~ "update" ]] || [[ $* =~ "outdated" ]] || [[ $* =~ "list" ]] || [[ $* =~ "install" ]] || [[ $* =~ "uninstall" ]] || [[ $* =~ "bundle" ]] || [[ $* =~ "doctor" ]] || [[ $* =~ "info" ]] || [[ $* =~ "cleanup" ]]; then
+  #       # If so, notify SketchyBar to trigger a custom action.
+  #       sketchybar --trigger brew_update
+  #     fi
+  #   }
+  # fi
   # ###########################################################################
-
-
-  # ###########################################################################
-  # SECTION: Luaver
-  # luaver can be used to install multiple lua and luarocks versions
-  # Commands below downloads and uses a specific version
-  # my_lua_touse=5.1 && luaver install $my_lua_touse && luaver set-default $my_lua_touse && luaver use $my_lua_touse
-  # my_luar_touse=3.11.0 && luaver install-luarocks $my_luar_touse && luaver set-default-luarocks $my_luar_touse && luaver use-luarocks $my_luar_touse
-  # luarocks install magick
-  # luaver install 5.4.6
-  #
-  # This is in case luaver was installed through homebrew
-  # If the file is not empty, then source it
-  [ -s $(brew --prefix)/opt/luaver/bin/luaver ] && . $(brew --prefix)/opt/luaver/bin/luaver
-  # This is in case it the repo was cloned with the following command
-  # git clone https://github.com/DhavalKapil/luaver.git ~/.luaver
-  # If the file is not empty, then source it
-  [ -s ~/.luaver/luaver ] && . ~/.luaver/luaver
-  # Line below won't work with zsh, there's no zsh completions I guess
-  # [ -s ~/.luaver/completions/luaver.bash ] && . ~/.luaver/completions/luaver.bash
-  # ###########################################################################
-
 
   # ###########################################################################
   # SECTION: fzf
@@ -282,18 +247,16 @@ if [ "$OS" = 'Mac' ]; then
   fi
   # ###########################################################################
 
-
   # ###########################################################################
   # SECTION: Bat -> Cat with wings
-  # if command -v bat &>/dev/null; then
-  #   # --style=plain - removes line numbers and git modifications
-  #   # --paging=never - doesnt pipe it through less
-  #   alias cat='bat --paging=never --style=plain'
-  #   alias catt='bat'
-  #   alias cata='bat --show-all --paging=never'
-  # fi
+  if command -v bat &>/dev/null; then
+    # --style=plain - removes line numbers and git modifications
+    # --paging=never - doesnt pipe it through less
+    alias cat='bat --paging=never --style=plain'
+    alias catt='bat'
+    alias cata='bat --show-all --paging=never'
+  fi
   # ###########################################################################
-
 
   # ###########################################################################
   # SECTION: zsh-syntax-highlighting
@@ -303,7 +266,6 @@ if [ "$OS" = 'Mac' ]; then
     source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
   fi
   # ###########################################################################
-
 
   # ###########################################################################
   # SECTION: z cd replacement
@@ -317,7 +279,6 @@ if [ "$OS" = 'Mac' ]; then
   fi
   # ###########################################################################
 
-
   # ###########################################################################
   # SECTION: MySQL
   # Add MySQL client to PATH, if it exists
@@ -325,79 +286,21 @@ if [ "$OS" = 'Mac' ]; then
     export PATH="/opt/homebrew/opt/mysql-client/bin:$PATH"
   fi
   # ###########################################################################
-
-  # ###########################################################################
-  # SECTION: Google Cloud SDK configurations
-  if command -v brew &>/dev/null; then
-    if [ -f "$(brew --prefix)/share/google-cloud-sdk/path.zsh.inc" ]; then
-      source "$(brew --prefix)/share/google-cloud-sdk/path.zsh.inc"
-    fi
-    if [ -f "$(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc" ]; then
-      source "$(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc"
-    fi
-  fi
-  # # Google Cloud SDK.
-  # if [ -f '/Users/adrio/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/adrio/google-cloud-sdk/path.zsh.inc'; fi
-  # if [ -f '/Users/adrio/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/adrio/google-cloud-sdk/completion.zsh.inc'; fi
-  # ###########################################################################
-
-
-  # ###########################################################################
-  # Initialize kubernetes kubectl completion if kubectl is installed
-  # https://kubernetes.io/docs/tasks/tools/install-kubectl-macos/#enable-shell-autocompletion
-  # if command -v kubectl &>/dev/null; then
-  #   source <(kubectl completion zsh)
-  # fi
-  # ###########################################################################
-
-
-  # ###########################################################################
-  # Check if chruby is installed
-  # Source chruby scripts if they exist
-  # Working instructions to install on macos can be found on the jekyll site
-  # https://jekyllrb.com/docs/installation/macos/
-  if [ -f "$(brew --prefix)/opt/chruby/share/chruby/chruby.sh" ]; then
-    source "$(brew --prefix)/opt/chruby/share/chruby/chruby.sh"
-    source "$(brew --prefix)/opt/chruby/share/chruby/auto.sh"
-    # Set default Ruby version using chruby
-    # Replace 'ruby-3.1.3' with the version you have or want to use
-    # You can also put a conditional check here if you want
-    chruby ruby-3.1.3
-  fi
-
+ 
   # ############################################################################
-  # SECTION: Misc config
-  ENABLE_CORRECTION="false"
-  COMPLETION_WAITING_DOTS="true"
-  export HOMEBREW_NO_AUTO_UPDATE="1"
-  export LANG=en_US.UTF-8
-  export PATH="$(brew --prefix)/bin:$PATH"
-  if command -v brew &>/dev/null; then
-    FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
-    autoload -Uz compinit
-    compinit
-  fi
+  # SECTION: powerlevel10k
   source $(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme
   [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-  unset ZSH_AUTOSUGGEST_USE_ASYNC
   # ############################################################################
 
-
-  # ###########################################################################
-  # SECTION: zsh-autosuggestions
-  # https://github.com/zsh-users/zsh-autosuggestions
-  # Right arrow to accept suggestion
-  if [ -f "$(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh" ]; then
-    source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-  fi
-  # ###########################################################################
 
   # #############################################################################
   # SECTION: private config
   if [ -f ~/github/dotfiles-latest/zshrc/helper/private.sh ]; then
     source ~/github/dotfiles-latest/zshrc/helper/private.sh
   fi
-
+  # #############################################################################
+  
 fi
 # ############################################################################
 
