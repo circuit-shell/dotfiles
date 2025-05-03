c0d42155f7832068d22affe879cd6f0c1d01969a
Armando del Río <adrio.6180@gmail.com>
2025-05-03 13:42:48 -0600

    fix: status line

--- neovim/specvim/lua/adrio/plugins/lsp/lspconfig.lua
+++ neovim/specvim/lua/adrio/plugins/lsp/lspconfig.lua
@@ -50,20 +50,13 @@ return {
 		-- Set up capabilities
 		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		-- Set up diagnostic signs
-		-- local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		-- for type, icon in pairs(signs) do
-		-- 	local hl = "DiagnosticSign" .. type
-		-- 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		-- end
-
 		vim.diagnostic.config({
 			signs = {
 				text = {
-					[vim.diagnostic.severity.ERROR] = "✘",
-					[vim.diagnostic.severity.WARN] = "▲",
-					[vim.diagnostic.severity.INFO] = "ℹ",
-					[vim.diagnostic.severity.HINT] = "⚑",
+					[vim.diagnostic.severity.ERROR] = " ",
+					[vim.diagnostic.severity.WARN] = " ",
+					[vim.diagnostic.severity.INFO] = "󰠠 ",
+					[vim.diagnostic.severity.HINT] = " ",
 				},
 			},
 		})
