9b07b2727af266655a3b71a677f6b413815436d3
Armando del Río <adrio.6180@gmail.com>
2025-11-12 18:36:15 -0600

    fix: merge code style

---
+++ neovim/csvim/lua/circuitshell/plugins/code-style.lua
@@ -0,0 +1,65 @@
+return {
+	{
+		"mfussenegger/nvim-lint",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local lint = require("lint")
+
+			lint.linters_by_ft = {
+				javascript = { "eslint_d" },
+				typescript = { "eslint_d" },
+				javascriptreact = { "eslint_d" },
+				typescriptreact = { "eslint_d" },
+				svelte = { "eslint_d" },
+				python = { "ruff" },
+				go = { "golangcilint" },
+			}
+
+			local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+				group = lint_augroup,
+				callback = function()
+					lint.try_lint()
+				end,
+			})
+
+			vim.keymap.set("n", "<leader>ll", function()
+				lint.try_lint()
+			end, { desc = "Trigger linting for current file" })
+		end,
+	},
+	{
+		"stevearc/conform.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local conform = require("conform")
+			conform.setup({
+				formatters_by_ft = {
+					javascript = { "prettier" },
+					typescript = { "prettier" },
+					javascriptreact = { "prettier" },
+					typescriptreact = { "prettier" },
+					svelte = { "prettier" },
+					css = { "prettier" },
+					html = { "prettier" },
+					json = { "prettier" },
+					yaml = { "yamlfmt" },
+					markdown = { "prettier" },
+					graphql = { "prettier" },
+					liquid = { "prettier" },
+					lua = { "stylua" },
+					go = { "goimports-reviser", "golines", "gofmt" },
+					python = { "isort", "black" },
+				},
+			})
+			vim.keymap.set({ "n", "v" }, "<leader>lf", function()
+				conform.format({
+					lsp_fallback = true,
+					async = false,
+					timeout_ms = 5000,
+				})
+			end, { desc = "Format file" })
+		end,
+	},
+}
